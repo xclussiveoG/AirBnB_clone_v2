ae86ec6440baad9be6967122282ebe6ea1245f62
xclussiveoG <ikireziremymagnom@gmail.com>
2024-05-19 12:58:38 -0700

    7-states_list.py

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Start web application with two routings
+"""
+
+from models import storage
+from models.state import State
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/states_list')
+def states_list():
+    """Render template with states
+    """
+    path = '7-states_list.html'
+    states = storage.all(State)
+    # sort State object alphabetically by name
+    sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return render_template(path, sorted_states=sorted_states)
